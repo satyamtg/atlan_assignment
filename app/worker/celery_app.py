817dbe7e8b05520e134f7a8908d3097259793788
Satyam Kumar <io.satyamtg@gmail.com>
2020-09-28 17:05:30 +0530

    Minor updates

--- app/worker/celery_app.py
+++ app/worker/celery_app.py
@@ -20,6 +20,6 @@ else: # running example with docker
         broker="amqp://user:bitnami@rabbitmq:5672//"
     )
     celery_app.conf.task_routes = {
-        "app.app.worker.celery_worker.test_celery": "test-queue"}
+        "app.worker.celery_worker.test_celery": "test-queue"}
 
 celery_app.conf.update(task_track_started=True)
